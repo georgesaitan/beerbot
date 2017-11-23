56c82286c5087751e3809ab7c0997f5f2b923317
georgesaitan <george.v.saitan@gmail.com>
2017-11-23 22:09:37 +0200

    first bug

--- bbv1.py
+++ bbv1.py
@@ -19,6 +19,10 @@ curses.cbreak()
 screen.keypad(True)
 
 
+def w1stop():
+    GPIO.output(in1, False)
+    GPIO.output(in2, False)
+
 def w1f():
     GPIO.output(in1, True)
     GPIO.output(in2, False)
@@ -29,13 +33,17 @@ def w1b():
     GPIO.output(in2, True)
 
 
+def w2stop():
+    GPIO.output(in3, True)
+    GPIO.output(in4, False)
+
 def w2f():
     GPIO.output(in3, True)
-    GPIO.output(in4, True)
+    GPIO.output(in4, False)
 
 
 def w2b():
-    GPIO.output(in3, True)
+    GPIO.output(in3, False)
     GPIO.output(in4, True)
 
 
@@ -47,10 +55,14 @@ try:
         elif char == ord('q'):
             w1f()
         elif char == ord('a'):
+            w1stop()
+        elif char == ord('z'):
             w1b()
         elif char == ord('w'):
             w2f()
         elif char == ord('s'):
+            w2stop()
+        elif char == ord('x'):
             w2b()
 
 finally:
